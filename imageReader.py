dbe7d27cf335371dbf812ca85cfa6573cdcaaa4e
davidgmorais <david_morais35@live.com.pt>
2021-11-08 18:18:05 +0000

    Implemented ObjectDetection on GUI

--- imageReader.py
+++ imageReader.py
@@ -1,73 +1,94 @@
-#!/usr/bin/python3
-
-# img_viewer.py
-
 import PySimpleGUI as sg
+import cv2
 import os.path
+from objectDetectionPictureOpenCV import ObjectDetection
 
-# First the window layout in 2 columns
 
-file_list_column = [
-    [
-        sg.Text("Image Folder"),
-        sg.In(size=(25, 1), enable_events=True, key="-FOLDER-"),
-        sg.FolderBrowse(),
-    ],
-    [
-        sg.Listbox(
-            values=[], enable_events=True, size=(40, 20), key="-FILE LIST-"
-        )
-    ],
-]
-
-# For now will only show the name of the file that was chosen
-image_viewer_column = [
-    [sg.Text("Choose an image from list on left:")],
-    [sg.Text(size=(40, 1), key="-TOUT-")],
-    [sg.Image(key="-IMAGE-")],
-]
-
-# ----- Full layout -----
-layout = [
-    [
-        sg.Column(file_list_column),
-        sg.VSeperator(),
-        sg.Column(image_viewer_column),
-    ]
-]
-
-window = sg.Window("Image Viewer", layout)
-
-# Run the Event Loop
-while True:
-    event, values = window.read()
-    if event == "Exit" or event == sg.WIN_CLOSED:
-        break
-    # Folder name was filled in, make a list of files in the folder
-    if event == "-FOLDER-":
-        folder = values["-FOLDER-"]
-        try:
-            # Get list of files in folder
-            file_list = os.listdir(folder)
-        except:
-            file_list = []
-
-        fnames = [
-            f
-            for f in file_list
-            if os.path.isfile(os.path.join(folder, f))
-            and f.lower().endswith((".png", ".gif"))
-        ]
-        window["-FILE LIST-"].update(fnames)
-    elif event == "-FILE LIST-":  # A file was chosen from the listbox
-        try:
-            filename = os.path.join(
-                values["-FOLDER-"], values["-FILE LIST-"][0]
+def main():
+    # ----- Left column layout -----
+    file_list_column = [
+        [
+            sg.Text("Image Folder"),
+            sg.In(size=(25, 1), enable_events=True, key="-FOLDER-"),
+            sg.FolderBrowse(),
+        ],
+        [
+            sg.Listbox(
+                values=[], enable_events=True, size=(40, 20), key="-FILE LIST-"
             )
-            window["-TOUT-"].update(filename)
-            window["-IMAGE-"].update(filename=filename)
+        ],
+    ]
 
-        except:
-            pass
+    # ----- Right column layout -----
+    image_viewer_column = [
+        [sg.Text("Choose an image from list on left:")],
+        [sg.Text(size=(40, 1), key="-TOUT-")],
+        [sg.Image(key="-IMAGE-")],
+        [sg.Button("Trigger object detection", key='-detection-', visible=False)]
+    ]
 
-window.close()
+    # --------- Full layout ---------
+    layout = [
+        [
+            sg.Column(file_list_column),
+            sg.VSeperator(),
+            sg.Column(image_viewer_column),
+        ]
+    ]
+
+    window = sg.Window("Image Viewer", layout)
+    object_detector = ObjectDetection()
+    selected, is_detection_on = None, False
+
+    # Run the Event Loop
+    while True:
+        event, values = window.read()
+        if event == "Exit" or event == sg.WIN_CLOSED:
+            break
+
+        # Folder name was filled in, make a list of files in the folder
+        if event == "-FOLDER-":
+            folder = values["-FOLDER-"]
+            try:
+                # Get list of files in folder
+                file_list = os.listdir(folder)
+            except:
+                file_list = []
+
+            f_names = [
+                f
+                for f in file_list
+                if os.path.isfile(os.path.join(folder, f))
+                and f.lower().endswith((".png", ".gif"))
+            ]
+            window["-FILE LIST-"].update(f_names)
+
+        # A file was chosen from the listbox
+        elif event == "-FILE LIST-":
+            try:
+                filename = os.path.join(
+                    values["-FOLDER-"], values["-FILE LIST-"][0]
+                )
+                selected, is_detection_on = filename, False
+                window["-TOUT-"].update(filename)
+                window["-IMAGE-"].update(filename=filename)
+                window["-detection-"].update(visible=True)
+            except:
+                pass
+
+        # Detection button was clicked
+        elif event == "-detection-" and selected:
+            if is_detection_on:
+                is_detection_on = False
+                window["-IMAGE-"].update(filename=selected)
+            else:
+                is_detection_on = True
+                output = object_detector.detect(selected)
+                imgbytes = cv2.imencode('.png', output)[1].tobytes()  # ditto
+                window["-IMAGE-"].update(data=imgbytes)
+
+    window.close()
+
+
+if __name__ == "__main__":
+    main()
--- objectDetectionPictureOpenCV.py
+++ objectDetectionPictureOpenCV.py
@@ -1,37 +1,37 @@
 #!/usr/bin/python3
-import cv2.cv2
 import cv2
 
-img = cv2.imread('/home/edkami/UA/Biometrics /Github_project/mais/lena.png')
 
-#cap = cv2.VideoCapture(-1)
-#cap.set(3, 640)  #
-#cap.set(4, 480)  #
+class ObjectDetection:
+    # init the Object Detection instance with the configurations
+    def __init__(self):
+        self.class_names = []
+        self.class_file = 'objectDetectorConf/coco.names'
+        self.config_path = 'objectDetectorConf/ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
+        self.weight_path = 'objectDetectorConf/frozen_inference_graph.pb'
 
-classNames = []
+        with open(self.class_file, 'rt') as file:
+            self.class_names = file.read().rstrip('\n').split('\n')
+            file.close()
 
-classFile = 'coco.names'
-with open(classFile, 'rt') as f:
-    classNames = f.read().rstrip('\n').split('\n')
+        self.net = cv2.dnn_DetectionModel(self.weight_path, self.config_path)
+        self.net.setInputSize((320, 320))
+        self.net.setInputScale(1.0 / 127.5)
+        self.net.setInputMean((127.5, 127.5, 127.5))
+        self.net.setInputSwapRB(True)
 
-configPath = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
-weightPath = 'frozen_inference_graph.pb'
+    # detect objects on an image, give the filename returns an image with a the name and a
+    # rectangle surrounding the objects detected drawn on it
+    def detect(self, file_name):
+        img = cv2.imread(file_name)
+        class_ids, confs, bbox = self.net.detect(img, confThreshold=0.5)
 
-net = cv2.dnn_DetectionModel(weightPath, configPath)
-net.setInputSize(320, 320)
-net.setInputScale(1.0 / 127.5)
-net.setInputMean((127.5, 127.5, 127.5))
-net.setInputSwapRB(True)
+        print(class_ids, bbox)
+        if len(class_ids) != 0:  #
+            for class_id, confidence, box in zip(class_ids.flatten(), confs.flatten(), bbox):
+                cv2.rectangle(img, box, color=(0, 255, 0), thickness=2)
+                cv2.putText(img, self.class_names[class_id - 1].upper(), (box[0] + 10, box[1] + 30),
+                            cv2.FONT_HERSHEY_COMPLEX, 1,
+                            (0, 255, 0), 2)
 
-#while True:  #
-    #success, img = cap.read()  #
-classIds, confs, bbox = net.detect(img, confThreshold=0.5)
-print(classIds, bbox)
-if len(classIds) != 0:  #
-    for classId, confidence, box in zip(classIds.flatten(), confs.flatten(), bbox):
-        cv2.rectangle(img, box, color=(0, 255, 0), thickness=2)
-        cv2.putText(img, classNames[classId - 1].upper(), (box[0] + 10, box[1] + 30), cv2.FONT_HERSHEY_COMPLEX, 1,
-                        (0, 255, 0), 2)
-
-cv2.imshow('output', img)
-cv2.waitKey(0)
+        return img
